886177ef24248dc721a8f40cedc1ca2cc46201fd
Sarah-delobelle <48509020+Sarah-delobelle@users.noreply.github.com>
2019-11-13 15:41:07 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+function ouvrir() {
+  document.getElementById("Navigation").style.width = "20%";
+}
+
+function fermer() {
+  document.getElementById("Navigation").style.width = "0%";
+}
+
+$('#sousmenu').fadeIn('fast');
+$('#sousmenu').fadeOut('slow');
